34ea3829fca1128e059873642e7defc5e455e12f
VitaliyD <TheWalkSideMail@gmail.com>
2022-07-12 15:06:59 +0300

    feat: add routing structure
    1. Инсталировал react router с помощью команды "npm i react-router-dom";
    2. Добавил 3 роута согласно заданию (Главная, авторизация, личный кабинет);
    3. Добавил 1 роут, который сигнализирует о ненайденной странице;
    4. Прописал линки для перехода на роуты.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import PersonalArea from './pages/PersonalAreaPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={'App'}>
+      <header>
+        <Link to="/">link home</Link>
+        <Link to="/login">link login</Link>
+        <Link to="/personal">link personal</Link>
+      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/personal"
+          element={<PersonalArea />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/PersonalAreaPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PersonalAreaPage = () => {
+  return (
+    <div>
+      PersonalAreaPage
+    </div>
+  );
+}
+
+export default PersonalAreaPage;
