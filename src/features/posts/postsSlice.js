9843419f25cc8496cd5fe4be9477e3cc30c7341a
VitaliyD <TheWalkSideMail@gmail.com>
2022-07-16 11:55:26 +0300

    feat: implement saving the list of posts
    При помощи localStorage реализовал сохранение списка постов при добавлении и удалении в домене/протоколе.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,11 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuid } from 'uuid';
 
 const initialState = {
-  postList: [
-    { id: uuid(), title: 'JavaScript 1', description: 'JavaScript is very cool' },
-    { id: uuid(), title: 'JavaScript 2', description: 'JavaScript is very cool' },
-    { id: uuid(), title: 'JavaScript 3', description: 'JavaScript is very cool' },
-  ],
+  postList: JSON.parse(localStorage.getItem('postList')),
 };
 
 const postsSlice = createSlice({
@@ -15,9 +11,11 @@ const postsSlice = createSlice({
   reducers: {
     addPost: (state, action) => {
       state.postList.push(action.payload);
+      localStorage.setItem('postList', JSON.stringify(state.postList));
     },
     deletePost: (state, action) => {
       state.postList.splice(action.payload - 1, 1);
+      localStorage.setItem('postList', JSON.stringify(state.postList));
     },
   },
 });
