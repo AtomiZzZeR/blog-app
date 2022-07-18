21d7ae4684c126b1d70ea8486bd929a2cf28c1c5
VitaliyD <TheWalkSideMail@gmail.com>
2022-07-18 11:11:50 +0300

    fix: fixed display of posts
    Sort the rendering array of posts.
    
    Because the posts were not displayed in the order they were added.

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,17 +3,19 @@ import Post from '../Post/Post';
 import Styled from './PostList.styles';
 import { useSelector } from 'react-redux';
 import { selectPosts } from '../../features/posts/postsSlice';
+import _ from 'lodash';
 
 const PostList = () => {
   const postsSelector = useSelector(selectPosts);
-
   const { postList } = postsSelector;
+
+  const sortedPostList = _.sortBy(postList, 'creatingData');
 
   return (
     <Styled.Wrapper>
       <Styled.Title>Список постов</Styled.Title>
       {
-        postList.map((post, index) =>
+        sortedPostList.map((post, index) =>
           <Post post={post} number={index + 1} key={index} />
         )
       }
