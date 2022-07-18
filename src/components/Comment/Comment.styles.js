73bab939fd06c6064696075ae6f74a6ed8685526
VitaliyD <TheWalkSideMail@gmail.com>
2022-07-18 11:13:13 +0300

    style: customize comment system

---
+++ src/components/Comment/Comment.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 2px dashed #ff6b6b;
+  padding: 15px 30px;
+  margin: 30px;
+`;
+
+const BtnForDelete = styled.button``;
+
+const StyledComment = {
+  Wrapper,
+  BtnForDelete,
+};
+
+export default StyledComment;
---
+++ src/components/CommentSystem/CommentSystem.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+const Figure = styled.div`
+  width: 100px;
+  height: 10px;
+  border: 2px dashed #222;
+  background-color: red;
+`;
+
+const StyledCommentSystem = {
+  Wrapper,
+  Figure,
+};
+
+export default StyledCommentSystem;
